d8b2fa020c5e9119ec1f23614a244a5eab756747
Hasti Sutaria <69644003+HastiSutaria@users.noreply.github.com>
2021-04-24 20:08:43 +0530

    Add files via upload

---
+++ rolldice.py
@@ -0,0 +1,44 @@
+import random
+x = "yes"
+while x == "yes":
+    no = random.randint(1,6)
+    0  
+    if no == 1:
+        
+        print("[     ]")
+        print("[  0  ]")
+        print("[     ]")
+       
+    if no == 2:
+        
+        print("[ 0   ]")
+        print("[     ]")
+        print("[   0 ]")
+       
+    if no == 3:
+        
+        print("[     ]")
+        print("[0 0 0]")
+        print("[     ]")
+        
+    if no == 4:
+        
+        print("[0   0]")
+        print("[     ]")
+        print("[0   0]")
+        
+    if no == 5:
+        
+        print("[0   0]")
+        print("[  0  ]")
+        print("[0   0]")
+        
+    if no == 6:
+        
+        print("[0 0 0]")
+        print("[     ]")
+        print("[0 0 0]")
+        
+          
+    x=input("type yes to roll again and 0 to exit:")
+    print("\n")
